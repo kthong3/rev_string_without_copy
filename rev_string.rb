814b0fcd9b38adb2dc4c6f83bf27687e8c01d6d1
kthong3 <kthongsakounh@gmail.com>
2018-03-20 13:02:36 -0500

    refactor

--- rev_string.rb
+++ rev_string.rb
@@ -7,42 +7,28 @@
 # move the letter to the front of the string
 # until you reach the beginning of the string
 
-# def rev_string(string)
-#   reversed = ""
-#   i = string.length - 1
-#   while i >= 0
-#     reversed += string[i]
-#     i -= 1
-#   end
-#   reversed
-# end
 
 hello = "hello"
-# hello = rev_string(hello)
-goodluck = "goodluck"
+timeline = "timeline"
+friends = "friends"
 
 def rev_string(string)
-  return string if string.length == 1
+  if string.length == 1
+    return string
+  end
   reversed = string
+  i = 0
+  j = string.length - 1
 
-  if string.length.odd?
-    i = string.length - 1
-    j = 0
-    while i > string.length / 2
-      reversed[i], reversed[j] = reversed[j], reversed[i]
-      i -= 1
-      j += 1
-    end
-  else
-    i = string.length - 1
-    j = 0
-    until i < string.length / 2
-      reversed[i], reversed[j] = reversed[j], reversed[i]
-      i -= 1
-      j += 1
-    end
+  while i < string.length/2
+    reversed[i], reversed[j] = reversed[j], reversed[i]
+    i += 1
+    j -= 1
   end
   reversed
 end
 
-p rev_string(goodluck)
+# p rev_string(hello)
+# rev_string(timeline)
+
+p rev_string(friends)
